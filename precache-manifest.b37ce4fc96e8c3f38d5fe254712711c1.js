e847110c248ef623aeba17da13aa2130b703a054
AbstractGandalf <shreyas.llama@gmail.com>
2018-12-18 21:03:11 +0530

    Updates

---
+++ precache-manifest.b37ce4fc96e8c3f38d5fe254712711c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e969e8e5f8dfe60a861",
+    "url": "/hxh-quotes/static/css/main.18b9afb9.chunk.css"
+  },
+  {
+    "revision": "1e969e8e5f8dfe60a861",
+    "url": "/hxh-quotes/static/js/main.1e969e8e.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/hxh-quotes/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "07196d0847335ff94226",
+    "url": "/hxh-quotes/static/js/runtime~main.07196d08.js"
+  },
+  {
+    "revision": "7da479e23c183d0a8e2876e84da4ed1f",
+    "url": "/hxh-quotes/index.html"
+  }
+];
